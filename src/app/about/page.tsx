c69b4ffe8f96f27f8d368b2eab02fa84ad7b97ff
Alan <alangialya@gmail.com>
2024-02-28 21:29:37 -0800

    Big UI changes, refactoring

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,86 +1,77 @@
-'use client'
+"use client";
 import React from "react";
-import { useRouter } from 'next/navigation'
-import avatar from '../../../public/avatar.png'
-import Image from 'next/image'
+import avatar from "../../../public/avatar.png";
+import Image from "next/image";
 import Header from "@/components/header";
-import MediaQuery from 'react-responsive'
-import about from "./about.json"
+import MediaQuery from "react-responsive";
+import About from "../../../public/about.json";
 import Footer from "@/components/footer";
+import Potato from "../../../public/photos/Potato.jpeg"
+import Pointing from "../../../public/photos/Pointing.jpeg"
+import Gum from "../../../public/photos/Gum.jpeg"
 
-
-function Page() {   
-    const Entry = (header: String, data: string[]) => {
-        return (
-            <div>
-                <p className="font-bold">{header}</p>
-                <ul>
-                    {data.map((li, index) => (
-                        <li key={index} className="w-30">{li}</li>
-                    ))}
-                </ul>
-                <br/>
-            </div>
-        )
-    }
-
-    const DesktopView = () => (
-        <div>
-            {/* First column */}
-            <div className="flex flex-row flex-wrap">
-                <div className="mt-10 ml-10">
-                    <Image src={avatar} alt="avatar" width={300} height={300}/>
-                </div>
-                <div className="flex flex-col mx-10 mt-10 w-1/4 lg:text-xl md:text-md">
-                    {Entry(about.intro.header, about.intro.list)}
-                    {Entry(about.learn.header, about.learn.list)}
-                    {Entry(about.goals.header, about.goals.list)}
-                </div>
-
-                {/* Second column */}
-                <div className="flex flex-col w-1/4 mx-10 mt-10 lg:text-xl md:text-md">
-                    {Entry(about.travel.header, about.travel.list)}
-                    {Entry(about.hobbies.header, about.hobbies.list)}
-                    {Entry(about.quote.header, about.quote.list)}
-                    {Entry(about.message.header, about.message.list)}
-                </div>
-            </div>
+function Page() {
+  const DesktopView = () => (
+    <div className="flex justify-center">
+      <div className="flex flex-col w-1/3">
+        <div className="flex flex-row mt-10">
+          <Image src={Potato} alt="Alan holding a potato tornado snack"height={300} className="hover:scale-110 hover:-translate-y-1 hover:rotate-12 duration-300"/>
+          <Image src={Pointing} alt="Alan pointing towards the ocean"height={300} className="ml-5 hover:scale-110 hover:-translate-y-1 hover:rotate-12 duration-300"/>
+          <Image src={Gum} alt="Alan at the Gum Wall"height={300} className="ml-5 hover:scale-110 hover:-translate-y-1 hover:rotate-12 duration-300"/>
         </div>
-    )
+        <ul>
+          {About.map((info, index) => (
+            <li className="my-5">
+							<div className="font-bold text-3xl">
+								{info.header}
+							</div>
+              <hr />
+							{info.list.map((item) => (
+                <p className="text-xl">{item}</p>
+              ))}
+						</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 
-    const MobileView = () => (
-        <div>
-            {/* First column */}
-            <div className="flex flex-col items-center">
-                <div className="mt-10">
-                    <Image src={avatar} alt="avatar" width={200} height={200}/>
-                </div>
-                <div className="flex flex-col mx-10 mt-10 w-25 lg:text-xl md:text-md">
-                    {Entry(about.intro.header, about.intro.list)}
-                    {Entry(about.travel.header, about.travel.list)}
-                    {Entry(about.learn.header, about.learn.list)}
-                    {Entry(about.goals.header, about.goals.list)}
-                    {Entry(about.hobbies.header, about.hobbies.list)}
-                    {Entry(about.quote.header, about.quote.list)}
-                    {Entry(about.message.header, about.message.list)}
-                </div>
-            </div>
+  const MobileView = () => (
+    <div className="flex justify-center">
+      <div className="flex flex-col px-5">
+        <div className="flex flex-row mt-10">
+          <Image src={Potato} alt="Alan holding a potato tornado snack" height={200} />
+          <Image src={Pointing} alt="Alan pointing towards the ocean" height={200} className="ml-5"/>
+          <Image src={Gum} alt="Alan at the Gum Wall" height={200} className="ml-5"/>
         </div>
-    )
-    
-    return (
-        <div className="">
-            <Header></Header>
-            <MediaQuery maxWidth={998}>
-                <MobileView></MobileView>
-            </MediaQuery>
-            {/* Desktop or laptop */}
-            <MediaQuery minWidth={999}>
-                <DesktopView></DesktopView>
-            </MediaQuery>
-            <Footer></Footer>
-        </div>
-    )
+        <ul>
+          {About.map((info, index) => (
+            <li className="my-5">
+							<div className="font-bold text-2xl">
+								{info.header}
+							</div>
+							{info.list.map((item) => (
+                <p className="text-xl">{item}</p>
+              ))}
+						</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="">
+      <Header></Header>
+      <MediaQuery maxWidth={767}>
+        <MobileView></MobileView>
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <DesktopView></DesktopView>
+      </MediaQuery>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Page;
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,153 +1,115 @@
-'use client'
+"use client";
 
-// TODO: Do not use image imports
-import Header from '../../components/header'
-import DocApp from '../../../public/photos/DocApp.png'
-import BeTogether from '../../../public/photos/BeTogether.png'
-import Flightapp from '../../../public/photos/Flightapp.png'
-import MangTracker from '../../../public/photos/MangTracker.png'
-import Image, { StaticImageData } from 'next/image'
-import { useState } from 'react'
-import React from 'react'
-import MediaQuery from 'react-responsive'
-import Footer from '@/components/footer'
-import Projects from './projects.json'
+import Header from "../../components/header";
+import avatar from "../../../public/avatar.png";
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+import React from "react";
+import MediaQuery from "react-responsive";
+import Footer from "@/components/footer";
+import Projects from "../../../public/projects.json";
+import About from "../../../public/about.json"
 
-// Project item interface
-// TODO: Make into separate JSON file
-export interface Project {
-    name: string
-    desc: string
-    img: StaticImageData
-    link: string
-}
-
-// TODO: Modularize project items into JSON, extract JSON elements
 export default function Page() {
-    const temp = ["DocApp is a project created during DubHacks 2023, a hackathon at the University of Washington. It aims to create a web application that connects patients to their doctors. Doctors can prescribe patient ailments/medical conditions and patients are able to ask GPT-4 common questions about the issue.", 
-    "Javascript-based React Native front end application for iOS and Android. We implemented technologies such as Neo4j (graph database), Key-Value Database AWS S3, and React.js.",
-    "Flightapp is a Java program that interacts with an Azure SQL database using JDBC (Java Database Connectivity) for retrieval, modification, and storage. It uses a secure create and login system with password salting and hashing, ensuring the protection of sensitive data. The app has a reservation functionality, enabling users to reserver itineraries and manage their bookings.",
-    "Mangtracker is a web application that lets users keep track of their favorite manga or manwha (Japanese/Korean) reads. It uses TypeScript, React, and Tailwind to create a responsive user interface. It fetches and displays relevant data about a series, including manga/manwha details, chapters, and release dates, providing up-to-date information to users."]
-    const [selectedProject, setSelectedProject] = useState(0)
-    const [projects, addProject] = useState<Project[]>([
-        {name: "DocApp", desc: temp[0], img:DocApp, link: "https://docapp-inky.vercel.app/"},
-        {name: "BeTogether", desc: temp[1], img:BeTogether, link: ""},
-        {name: "FlightApp", desc: temp[2], img:Flightapp, link: ""},
-        {name: "MangTracker", desc: temp[3], img:MangTracker, link: ""},
-    ])
+  const [selectedProject, setSelectedProject] = useState<number | null>(null);
+  const [jsonProjects] = useState(Projects);
 
-    const [jsonProjects, addJsonProject] = useState(
-        [Projects]
-    )
+  // Project selection handler
+  const handleSelection = (id: number) => {
+    if (id === selectedProject) {
+      setSelectedProject(null);
+    } else if (id) {
+      setSelectedProject(id);
+    }
+  };
 
-    console.log(jsonProjects)
+	const handleButtonClick = (link: string) => {
     
-    // Project selection handler
-    const handleSelection = (id: number) => {
-        if (id === selectedProject) {
-            setSelectedProject(0)
-        } else {
-            console.log(id)
-            setSelectedProject(id)
-        }
-    }
+  };
 
-    // Project image click handler (redirect)
-    const handleProjectImageClick = (link: string) => {
-        if (link === "") {
-            console.log("No link")
-        } else {
-            console.log("clicked")
-            window.open(link)
-        }
-    }
+  /**
+	 * The media view for mobile users.
+	 * @returns The mobile media view used within a MediaQuery component.
+	 */
+  const MobileView = () => (
+    <div className="flex flex-row flex-wrap">
+      <div className="flex flex-col mt-0 w-full text-center lg:text-xl md:text-md">
+        <p className='font-bold'>Here are some of my projects:</p>
+        <ul className="">
+          {jsonProjects.map((project, index) => (
+            <li className="my-10 font-medium text-black-500 " key={index}>
+              <button
+                onClick={() => handleSelection(index)}
+                className="hover:-translate-y-1 hover:scale-105 duration-300"
+              >
+                {project.name}
+              </button>
+              {selectedProject == index ? (
+                <div>
+                  <p className="my-5 mx-10 text-left">
+                    {jsonProjects[selectedProject].description}
+                  </p>
+                </div>
+              ) : (
+                <></>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 
-    // Mobile view render
-    const MobileView = () => (
-        <div className="flex flex-row flex-wrap">
-            <div className="flex flex-col mt-0 w-full text-center lg:text-xl md:text-md">
-                {/* <p className='font-bold'>Here are some of my projects:</p> */}
-                <ul className=''>
-                    {projects.map((project, index) => (
-                    <li className='my-10 font-medium text-black-500 ' key={index}>
-                        <button onClick={() => handleSelection(index)} className='hover:-translate-y-1 hover:scale-105 duration-300'>
-                            {project.name}                                    
-                        </button>
-                        {/* <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded dark:bg-gray-500"/> */}
-                        {selectedProject == index ?
-                            <div>
-                            <Image 
-                            src={projects[selectedProject].img} 
-                            alt={"seattle"} 
-                            onClick={() => handleProjectImageClick(projects[selectedProject].link)} 
-                            className='w-full hover:opacity-80'
-                            height={200}/>
-                            <p className='my-5 mx-10 text-left'>{projects[selectedProject].desc}</p>
-                            </div>
-                            : <></>
-                            }
-                    </li>))}
-                </ul>
+  /**
+	 * The media view for desktop users.
+	 * @returns The desktop media view used within a MediaQuery component.
+	 */
+  const DesktopView = () => (
+    <div className="flex justify-center flex-row flex-wrap">
+      <div className="flex flex-col w-1/3 mt-10 mx-10 lg:text-xl md:text-md sm:text-md">
+        <div className="flex flex-row mt-10">
+					<div className="hover:scale-110 hover:-translate-y-1 hover:rotate-12 duration-300">
+          	<Image src={avatar} alt="avatar" width={300} height={300} />
+					</div>
+          <div className="flex flex-col ml-5">
+            <p className="font-bold">{About[0].header}</p>
+						<hr />
+            <ul>
+              {About[0].list.map((item, index) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <p className="my-5">Here are some of my projects:</p>
+        <ul>
+          {jsonProjects.map((project, index) => (
+            <div>
+              <a className="font-bold lg:text-4xl md:text-4xl sm:text-2xl hover:underline duration-300 "
+							href={project.link}
+							target="blank"
+							>
+                {project.name}
+              </a>
+							<hr className="mt-2"/>
+              <p className="my-5">{project.description}</p>
             </div>
-        </div>
-    )
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 
-    // Desktop view render
-    const DesktopView = () => (
-        <div className='flex flex-col justify-between'>
-
-        <div className="flex flex-row flex-wrap">
-            {/* First column */}
-            <div className="flex flex-col mt-10 w-1/4 ml-10 lg:text-xl md:text-md">
-                <p className='font-bold'>Here are some of my projects:</p>
-
-                {/* <ul>
-                    {projects.map((project, index) => (
-                    <li className='my-10 font-medium text-black-500 hover:-translate-y-1 hover:scale-105 duration-300' key={index}>
-                        <button onClick={() => handleSelection(index)}>
-                            {project.name}
-                        </button>
-                    </li>
-                    ))}
-                </ul> */}
-                <ul>
-                    
-                </ul>
-            </div>
-
-            {/* Second column */}
-            <div className="bg-fixed w-1/2 mt-10 mx-10 lg:text-xl md:text-md sm:text-md">
-                {/* <Image 
-                    src={projects[selectedProject].img} 
-                    alt={"seattle"} 
-                    onClick={() => handleProjectImageClick(projects[selectedProject].link)} 
-                    className='hover:-translate-y-1 hover:scale-105 duration-300 hover:opacity-80'
-                    height={100}/> */}
-                <p className='font-bold lg:text-8xl md:text-6xl sm:text-4xl'>{projects[selectedProject].name}</p>
-                <p className='my-5'>{projects[selectedProject].desc}</p>
-            </div>
-        </div>
-        </div>
-
-    )
-
-    return (
-        <div>
-            {/* Header */}
-            <Header></Header>
-
-            {/* Mobile view */}
-            <MediaQuery maxWidth={767}>
-                <MobileView></MobileView>
-            </MediaQuery>
-
-            {/* Desktop or laptop */}
-            <MediaQuery minWidth={768}>
-                <DesktopView></DesktopView>
-            </MediaQuery>
-
-            <Footer></Footer>
-            
-        </div>
-    )
-  }
+  return (
+    <div>
+      <Header></Header>
+      <MediaQuery maxWidth={767}>
+        <MobileView></MobileView>
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <DesktopView></DesktopView>
+      </MediaQuery>
+      <Footer></Footer>
+    </div>
+  );
+}
